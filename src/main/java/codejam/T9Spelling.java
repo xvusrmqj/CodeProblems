4dc98316bcb2f770f00362746ce801c1fc93a1ca
xvusrmqj <olv@foxmail.com>
2018-12-01 14:57:52 +0800

    first commit

---
+++ src/main/java/codejam/T9Spelling.java
@@ -0,0 +1,9 @@
+package codejam;
+
+public class T9Spelling {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/main/java/剑指offer/aModule.java
@@ -0,0 +1,20 @@
+package 剑指offer;
+/**
+ * 
+ * 
+ * @author lv
+ *
+ */
+public class aModule {
+
+	public static void main(String[] args) {
+		int[] arr = {};
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(fun());
+		}
+	}
+	public static int fun() {
+		return 0;
+	}
+
+}
