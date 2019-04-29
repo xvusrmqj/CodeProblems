dc67134d98fe5ee72750897366b329eaf8434f11
xvusrmqj <olv@foxmail.com>
2019-04-30 07:24:18 +0800

    添加注释

--- src/main/java/互联网公司/头条/大数相加.java
+++ src/main/java/互联网公司/头条/大数相加.java
@@ -9,23 +9,27 @@ import org.junit.Test;
 public class 大数相加 {
     // 6:49 - 7:08 - 7:18
     public String add(String s1, String s2) {
-        int mov = 0;
-        StringBuilder sb = new StringBuilder();
+        int mov = 0;// 进位
+        StringBuilder sb = new StringBuilder();// 保存结果
+        // 循环遍历，从尾到头
         for (int i = s1.length()-1, j = s2.length()-1; i >= 0 || j >= 0; i--, j--) {
+            // 当为下标负数时，说明已经不需要它了。为了减少判断设为0
             int s1_int;
             if (i < 0) {
                 s1_int = 0;
             } else {
                 s1_int = s1.charAt(i) - '0';
             }
+            // 当为下标负数时，说明已经不需要它了。为了减少判断设为0
             int s2_int;
             if (j < 0) {
                 s2_int = 0;
             } else {
                 s2_int = s2.charAt(j) - '0';
             }
-
+            // 两数相加再加进位
             int s1_s2 = s1_int + s2_int + mov;
+            // 使用进位后将进位再设置回默认值
             mov = 0;
             if (s1_s2 > 9) {
                 mov = 1;
@@ -34,6 +38,7 @@ public class 大数相加 {
                 sb.append(s1_s2);
             }
         }
+        // 最后一个进位如果有值就加上。
         if(mov==1)
             sb.append(mov);
         return sb.reverse().toString();
