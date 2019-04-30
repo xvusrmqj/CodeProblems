9a6bbee68ed387dbe010755330981fe4efbe8e12
lvxg <olv@foxmail.com>
2019-04-30 17:36:23 +0800

    add

---
+++ src/main/java/互联网公司/头条/二叉树翻转.java
@@ -0,0 +1,25 @@
+package 互联网公司.头条;
+
+/**
+ * 要求：不能使用递归
+ */
+public class 二叉树翻转 {
+    // 二叉树定义
+    class Node{
+        Node left;
+        Node right;
+        int val;
+
+        public Node(Node left, Node right, int val) {
+            this.left = left;
+            this.right = right;
+            this.val = val;
+        }
+
+        @Override
+        public String toString() {
+            return null;
+        }
+    }
+
+}
